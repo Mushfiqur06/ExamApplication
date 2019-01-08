9047aa5ba73aaac83454f87a4d54b9a7c5e2da1c
mushfiqur06 <mushfiqur06@gmail.com>
2019-01-09 01:25:55 +0600

    Add thirparty module, create middlewares and create a server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//PORT 
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log('Server is on Fire...');
+    mongoose.connect('mongodb://localhost:27017/exam-application', {useNewUrlParser: true}, () => {
+        console.log('Database running... ')
+    });
+})
