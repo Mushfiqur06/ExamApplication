70706c19e64300ee5d77988580b894cb0cd9bb65
mushfiqur06 <mushfiqur06@gmail.com>
2019-01-09 03:01:23 +0600

    Make userController

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcrypt')
+const User =require('../models/User');
+const registrationValidate = require('../validators/registrationValidator')
+
+module.exports = {
+    register(req, res){
+        const {name, email, password, confirmPassword} = req.body;
+
+        const result = registrationValidate({name, email, password, confirmPassword})
+
+        if(!result.isValid){
+            res.status(400).json(result.error)
+        }else{
+            res.status(200).json({
+                message: 'Everything is ok';
+            })
+        }
+    }
+}
