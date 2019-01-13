39722843de511009237dd500d751c7d707231c9e
mushfiqur06 <mushfiqur06@gmail.com>
2019-01-13 13:22:33 +0600

    user activate code router add

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { register, activeAccount, getAllUsers } =require('../controllers/userController');
 
 router.post('/register', register);
-router.get('/activateaccount/:token', activeAccount);
+router.post('/activateaccount/:token', activeAccount);
 router.get('/all', getAllUsers)
 
 module.exports = router;
