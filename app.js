c3ccfc30a94aacb90b637b28eedb2790fbd3070a
mushfiqur06 <mushfiqur06@gmail.com>
2019-01-13 13:20:25 +0600

    Add MongoDB server from mlab

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//Connect Database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://exampApplicationUser:exmapp123@ds255784.mlab.com:55784/examapplication', {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', () => {
+    console.log('Database Not Connected');
+});
+
+db.once('open', () => {
+    console.log('Database Connected');
+})
+
 //Middlewares
 app.use(cors());
 app.use(morgan('dev'));
@@ -17,9 +29,4 @@ app.use('/api/users', require('./routes/userRoutes'))
 
 //PORT
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log('Server is on Fire...');
-    mongoose.connect('mongodb://localhost:27017/exam-application', {useNewUrlParser: true}, () => {
-        console.log('Database running... ')
-    });
-})
+app.listen(PORT, () => console.log('Server is on Fire...'))
